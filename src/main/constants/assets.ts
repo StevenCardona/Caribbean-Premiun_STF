aca25201bb87924f8d51cb5ff3765b69c2099115
Steven Cardona <steven.cardona@platec.co>
2023-10-03 00:16:03 -0500

    Section 1: Print Etnico | Complete-Parcial

--- src/main/constants/assets.ts
+++ src/main/constants/assets.ts
@@ -1,8 +1,8 @@
 export const IMG_ROUTES = {
     titleBanner: '/assets/images/DESKTOP/Grupo 11.png',
     logoBanner: '/assets/images/DESKTOP/Grupo 22.png',
-    section1Main: '/assets/images/DESKTOP/I2.png',
+    section1Main: '/assets/images/DESKTOP/I2.jpg',
     section1Main_MB: '/assets/images/MOBILE/I2.jpg',
-    section1Second: '/assets/images/DESKTOP/I3.png',
+    section1Second: '/assets/images/DESKTOP/I3.jpg',
     section1Second_MB: '/assets/images/MOBILE/I3.jpg',
 };
--- src/ui/components/sections/print-etnico/PrintEtnicoSection.tsx
+++ src/ui/components/sections/print-etnico/PrintEtnicoSection.tsx
@@ -1,3 +1,50 @@
+import { isDesktop } from 'react-device-detect';
+import { IMG_ROUTES } from '../../../../main/constants';
+import { COPYS_ES } from '../../../../main/resources';
+import { CustomButton } from '../../buttons/custom-button';
+import './styles.css';
+
 export const PrintEtnicoSection = () => {
-    return <div className='section'>PrintEtnicoSection</div>;
+    return (
+        <div className='section d-lg-flex gap-5'>
+            <div className='d-flex flex-column-reverse flex-column-reverse section__imgs flex-lg-row align-items-lg-center'>
+                <div className='section__thumbnail d-flex justify-content-center align-items-center mt-4 mt-lg-0'>
+                    <img
+                        src={
+                            isDesktop
+                                ? IMG_ROUTES.section1Second
+                                : IMG_ROUTES.section1Second_MB
+                        }
+                        alt='mujer vestida con un vestido etnico y sostiene un bolso con su mano'
+                        className='section__above'
+                    />
+                </div>
+
+                <img
+                    className='section__img'
+                    src={
+                        isDesktop
+                            ? IMG_ROUTES.section1Main
+                            : IMG_ROUTES.section1Main_MB
+                    }
+                    alt='mujer vestida con un vestido etnico y sostiene un bolso con su mano'
+                />
+            </div>
+
+            <div className='section__box p-lg-4 text-center text-lg-start d-lg-flex flex-lg-column algin-items-center justify-content-center mt-5 mt-lg-0'>
+                <h2 className='section__title '>
+                    {COPYS_ES.print_etnico_section.title}
+                </h2>
+
+                <h4 className='section__subtitle'>
+                    {COPYS_ES.print_etnico_section.subtitle}
+                </h4>
+
+                <CustomButton
+                    classNames='d-none d-lg-block black mt-lg-5 xl'
+                    title={COPYS_ES.show_complements}
+                />
+            </div>
+        </div>
+    );
 };
