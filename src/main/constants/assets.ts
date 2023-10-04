47316824431386fbb5bf97f896bd576b99db49bb
Steven Cardona <steven.cardona@platec.co>
2023-10-03 23:41:45 -0500

    Rename urls for the images, to deploy page correctly

--- src/main/constants/assets.ts
+++ src/main/constants/assets.ts
@@ -1,47 +1,47 @@
 export const IMG_ROUTES = {
-    titleBanner: '/assets/images/DESKTOP/Grupo 11.png',
-    logoBanner: '/assets/images/DESKTOP/Grupo 22.png',
-    section1Main: '/assets/images/DESKTOP/I2.jpg',
-    section1Main_MB: '/assets/images/MOBILE/I2.jpg',
-    section1Second: '/assets/images/DESKTOP/I3.jpg',
-    section1Second_MB: '/assets/images/MOBILE/I3.jpg',
+    titleBanner: '/Caribbean-Premiun_STF/assets/images/DESKTOP/Grupo 11.png',
+    logoBanner: '/Caribbean-Premiun_STF/assets/images/DESKTOP/Grupo 22.png',
+    section1Main: '/Caribbean-Premiun_STF/assets/images/DESKTOP/I2.jpg',
+    section1Main_MB: '/Caribbean-Premiun_STF/assets/images/MOBILE/I2.jpg',
+    section1Second: '/Caribbean-Premiun_STF/assets/images/DESKTOP/I3.jpg',
+    section1Second_MB: '/Caribbean-Premiun_STF/assets/images/MOBILE/I3.jpg',
 
     collection_MB: {
-        img1: '/assets/images/MOBILE/I5.jpg',
-        img2: '/assets/images/MOBILE/I7.jpg',
-        img3: '/assets/images/MOBILE/I6.jpg',
-        img4: '/assets/images/MOBILE/I8.jpg',
+        img1: '/Caribbean-Premiun_STF/assets/images/MOBILE/I5.jpg',
+        img2: '/Caribbean-Premiun_STF/assets/images/MOBILE/I7.jpg',
+        img3: '/Caribbean-Premiun_STF/assets/images/MOBILE/I6.jpg',
+        img4: '/Caribbean-Premiun_STF/assets/images/MOBILE/I8.jpg',
     },
 
     collection: {
-        img1: '/assets/images/DESKTOP/I6.jpg',
-        img2: '/assets/images/DESKTOP/I7.jpg',
-        img3: '/assets/images/DESKTOP/I8.jpg',
-        img4: '/assets/images/DESKTOP/I9.jpg',
+        img1: '/Caribbean-Premiun_STF/assets/images/DESKTOP/I6.jpg',
+        img2: '/Caribbean-Premiun_STF/assets/images/DESKTOP/I7.jpg',
+        img3: '/Caribbean-Premiun_STF/assets/images/DESKTOP/I8.jpg',
+        img4: '/Caribbean-Premiun_STF/assets/images/DESKTOP/I9.jpg',
     },
 
-    collectionMain: '/assets/images/DESKTOP/I5.jpg',
+    collectionMain: '/Caribbean-Premiun_STF/assets/images/DESKTOP/I5.jpg',
 
     collection2_MB: {
-        img1: '/assets/images/MOBILE/I9.jpg',
-        img2: '/assets/images/MOBILE/I1.jpg',
+        img1: '/Caribbean-Premiun_STF/assets/images/MOBILE/I9.jpg',
+        img2: '/Caribbean-Premiun_STF/assets/images/MOBILE/I1.jpg',
     },
 
     collection3: {
-        img1: '/assets/images/DESKTOP/I12.jpg',
+        img1: '/Caribbean-Premiun_STF/assets/images/DESKTOP/I12.jpg',
     },
 
     collection3_MB: {
-        img1: '/assets/images/MOBILE/I11.jpg',
+        img1: '/Caribbean-Premiun_STF/assets/images/MOBILE/I11.jpg',
     },
 
     collection4: {
-        img1: '/assets/images/DESKTOP/I13.jpg',
-        img2: '/assets/images/DESKTOP/I14.jpg',
+        img1: '/Caribbean-Premiun_STF/assets/images/DESKTOP/I13.jpg',
+        img2: '/Caribbean-Premiun_STF/assets/images/DESKTOP/I14.jpg',
     },
 
     collection4_MB: {
-        img1: '/assets/images/MOBILE/I12.jpg',
-        img2: '/assets/images/MOBILE/I13.jpg',
+        img1: '/Caribbean-Premiun_STF/assets/images/MOBILE/I12.jpg',
+        img2: '/Caribbean-Premiun_STF/assets/images/MOBILE/I13.jpg',
     },
 };
